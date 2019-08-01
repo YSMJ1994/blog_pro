67e279fdb98d7b61a2ed29e85c76ebea6e4b772f
zhaopengcheng <710411486@qq.com>
2019-08-01 17:03:50 +0800

    combine context provider util

---
+++ src/utils/combineCtxProvider.tsx
@@ -0,0 +1,17 @@
+import React, { FC, PropsWithChildren } from 'react';
+
+const combine = (providers: FC[]): FC => (props: PropsWithChildren<{}>) => {
+	const Provider = providers[0];
+	if (providers.length === 1) {
+		const Provider = providers[0];
+		return <Provider {...props} />;
+	} else {
+		const NextProvider = combine(providers.slice(1));
+		return (
+			<Provider>
+				<NextProvider {...props} />
+			</Provider>
+		);
+	}
+};
+export default combine;
