ef79be60115e0cca64477ddb7098f75fef883e74
zhaopengcheng <710411486@qq.com>
2019-08-01 17:16:17 +0800

    extract menu data to header comp and use github avatar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import Styles from './style.module.scss';
 import Search from '@/components/Search';
 import { Link, RouteComponentProps, withRouter } from 'react-router-dom';
 import SIcon from '@/components/SIcon';
 import CustomLink from '@/components/Link';
 import cs from 'classnames';
+import InfoCtx from '@/ctx/InfoCrx';
 
 interface MenuItemProps extends RouteComponentProps {
 	label: string;
@@ -25,7 +26,30 @@ const MenuItem = withRouter<MenuItemProps>(({ label, link, history, location }) 
 	);
 });
 
-function MenuList({ className, setPhoneShowMenu }: { className?: string; setPhoneShowMenu: (show: boolean) => void }) {
+function MenuList({
+	className,
+	menu = [],
+	setPhoneShowMenu
+}: {
+	className?: string;
+	menu: { label: string; link: string }[];
+	setPhoneShowMenu: (show: boolean) => void;
+}) {
+	return (
+		<div className={cs(Styles.menuList, className)} onClick={() => setPhoneShowMenu(false)}>
+			<div className={Styles.menuMain} onClick={e => e.stopPropagation()}>
+				{menu.map(item => (
+					<MenuItem key={item.label} label={item.label} link={item.link} />
+				))}
+			</div>
+		</div>
+	);
+}
+
+function Header() {
+	const info = useContext(InfoCtx);
+	const { avatar_url } = info;
+	const [phoneShowMenu, setPhoneShowMenu] = useState(false);
 	const menu = [
 		{
 			label: '主页',
@@ -49,31 +73,17 @@ function MenuList({ className, setPhoneShowMenu }: { className?: string; setPhon
 		}
 	];
 	return (
-		<div className={cs(Styles.menuList, className)} onClick={() => setPhoneShowMenu(false)}>
-			<div className={Styles.menuMain} onClick={e => e.stopPropagation()}>
-				{menu.map(item => (
-					<MenuItem key={item.label} label={item.label} link={item.link} />
-				))}
-			</div>
-		</div>
-	);
-}
-
-function Header() {
-	const [phoneShowMenu, setPhoneShowMenu] = useState(false);
-	return (
 		<div className={Styles.header}>
 			<SIcon name="menu" className={Styles.menuIcon} onClick={() => setPhoneShowMenu(!phoneShowMenu)} />
 			<Link to="/" style={{ fontSize: 0 }}>
-				<img
-					title="Home"
-					alt="loading"
-					className={Styles.avatar}
-					src="https://avatars1.githubusercontent.com/u/18289306?s=460&v=4"
-				/>
+				<img title="Home" alt="loading" className={Styles.avatar} src={avatar_url} />
 			</Link>
 			<div className={Styles.name}>SoberZ</div>
-			<MenuList className={cs({ [Styles.menuShow]: phoneShowMenu })} setPhoneShowMenu={setPhoneShowMenu} />
+			<MenuList
+				className={cs({ [Styles.menuShow]: phoneShowMenu })}
+				menu={menu}
+				setPhoneShowMenu={setPhoneShowMenu}
+			/>
 			<Search />
 		</div>
 	);
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import DocCtx from '@/ctx/DocCtx';
 
 function Home() {
