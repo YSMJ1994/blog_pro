276628260a4d41954d06fe964db78df3131517ca
zhaopengcheng <710411486@qq.com>
2019-08-01 17:07:49 +0800

    combine info and doc context

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import '@/styles/global.scss';
 import 'highlight.js/styles/atom-one-dark.css';
 import Pages from '@/pages/Pages';
 import { load } from 'utils/loadIcon';
+import combineCtxProvider from 'utils/combineCtxProvider';
 load();
-import { Provider } from '@/ctx/DocCtx';
+import { Provider as DocProvider } from '@/ctx/DocCtx';
+import { Provider as InfoProvider } from '@/ctx/InfoCrx';
 
+const Provider = combineCtxProvider([DocProvider, InfoProvider]);
 const App: React.FC = () => {
 	return (
 		<div className="App">
