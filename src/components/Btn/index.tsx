950992d7dfeb5cb17ab88f85f608be6f8ead210e
zhaopengcheng <710411486@qq.com>
2019-09-16 17:09:55 +0800

    添加按钮组件

---
+++ src/components/Btn/index.tsx
@@ -0,0 +1,39 @@
+import React, { FC, PropsWithChildren, EventHandler, MouseEvent, CSSProperties } from 'react';
+import styles from './style.module.scss';
+import cs from 'classnames';
+
+type BtnProps = {
+	disabled?: boolean;
+	loading?: boolean;
+	type?: 'text' | 'normal' | 'success' | 'warn' | 'error';
+	fill?: boolean;
+	className?: string;
+	style?: CSSProperties;
+	onClick?: EventHandler<MouseEvent<HTMLButtonElement>>;
+};
+const Btn: FC<PropsWithChildren<BtnProps>> = ({
+	className,
+	style,
+	disabled = false,
+	loading = false,
+	fill = false,
+	type = 'normal',
+	children,
+	onClick
+}) => {
+	return (
+		<button
+			className={cs(styles.btn, styles[`btn--${type}`], className, {
+				[styles.btnDisabled]: disabled,
+				[styles.btnLoading]: loading,
+				[styles.btnFill]: fill
+			})}
+			style={style}
+			onClick={onClick}
+		>
+			{children}
+		</button>
+	);
+};
+
+export default Btn;
