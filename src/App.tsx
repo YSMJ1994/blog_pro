a7cfae2778a5df6feb0643e1e0980f433b7227c3
zhaopengcheng <710411486@qq.com>
2019-12-05 14:24:28 +0800

    moment语言设为中文

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,12 @@ import loadIcon from '@/utils/loadIcon';
 import combineCtxProvider from '@/utils/combineCtxProvider';
 import { Provider as DocProvider } from '@/ctx/DocCtx';
 import { Provider as InfoProvider } from '@/ctx/InfoCrx';
+import 'moment/locale/zh-cn';
+import Moment from 'moment';
 
+Moment.locale('zh-cn');
 loadIcon.load();
+
 const Provider = combineCtxProvider([DocProvider, InfoProvider]);
 const App: React.FC = () => {
 	return (
