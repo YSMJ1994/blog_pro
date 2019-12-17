f88434d475587575d3cfa6fb02d7e9a65265f4f2
zhaopengcheng <710411486@qq.com>
2019-12-17 14:48:48 +0800

    fix 隐含的计算时间过长导致线程阻塞及频繁搜索导致返回内容不正确的bug

--- src/utils/SearchWorker.ts
+++ src/utils/SearchWorker.ts
@@ -1,5 +1,4 @@
 import { DocInfo, Group, MdDoc, Tag } from '@/ctx/DocCtx';
-let worker: Worker | undefined;
 
 export interface SearchTag {
 	name: string;
@@ -22,20 +21,61 @@ export interface SearchResult {
 	articles: SearchArticle[];
 }
 
+const getWorker = (function() {
+	// 搜索脚本的路径
+	const scriptUrl = 'js/searchWorker.js';
+	// worker实例
+	let worker: Worker | undefined;
+	// worker状态： 1为空闲，0为繁忙或不可用
+	let workerStatus: 0 | 1 = 0;
+	// 上次任务的reject
+	let latestReject: ((reason?: any) => any) | undefined;
+
+	return () => {
+		// 未初始化或繁忙，则重置worker
+		if (!worker || !workerStatus) {
+			// 上次任务reject
+			latestReject && latestReject('有新的搜索任务');
+			// 销毁线程
+			worker && worker.terminate();
+			// 初始化worker
+			worker = new Worker(scriptUrl);
+			// 初始化状态
+			workerStatus = 1;
+		}
+		return {
+			worker,
+			status: workerStatus,
+			setStatus(status: 0 | 1) {
+				workerStatus = status;
+			},
+			setReject(reject: (reason?: any) => any) {
+				latestReject = reject;
+			}
+		};
+	};
+})();
+
 export default function SearchWorker(keyword: string, docInfo: DocInfo) {
 	return new Promise<SearchResult>((resolve, reject) => {
-		if (!worker) {
-			worker = new Worker('js/searchWorker.js');
-		}
-		// 2s秒后销毁线程，搜索失败
-		setTimeout(() => {
-			worker && worker.terminate();
-			worker = undefined;
+		const { worker, setStatus, setReject } = getWorker();
+		// 设置为繁忙
+		setStatus(0);
+		// 5s秒后超时
+		let timeout = setTimeout(() => {
 			reject('timeout');
-		}, 2000);
+			// 释放资源
+			worker && worker.terminate();
+		}, 5000);
+		// 监听消息
 		worker.onmessage = function(this: Worker, ev: MessageEvent) {
+			clearTimeout(timeout);
 			resolve(ev.data);
+			setStatus(1);
 		};
+		// 记录reject
+		setReject(reject);
+		// 发送消息
 		worker.postMessage({ keyword, docInfo });
 	});
 }
